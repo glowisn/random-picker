c7a24831c92d69df1f8af6c4272219582ef23f15
Glowisn <asdxcv1998@gmail.com>
2024-09-19 16:31:06 +0900

    add ad test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6827764378426795"
+        crossOrigin="anonymous"
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
