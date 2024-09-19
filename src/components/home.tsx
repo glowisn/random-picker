796c24ee2fac8e94b72c36fa3a90161df30b2e70
Glowisn <asdxcv1998@gmail.com>
2024-09-19 10:23:52 +0900

    add localStorage

---
+++ src/components/home.tsx
@@ -0,0 +1,9 @@
+import RandomPicker from "./randomPicker";
+
+export default function Home() {
+  return (
+    <div className="flex flex-row bg-gray-700 h-screen">
+      <RandomPicker />
+    </div>
+  );
+}
--- src/components/randomPicker.tsx
+++ src/components/randomPicker.tsx
@@ -1,20 +1,45 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function RandomPicker() {
   const [list, setList] = useState<string[]>([]);
   const [item, setItem] = useState<string>("");
-  const [result, setResult] = useState<string>("결과");
+  const [result, setResult] = useState<string>("");
+  const [lastResult, setLastResult] = useState<string>("");
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+    function handleStorageChange() {
+      setLastResult(localStorage.getItem("lastResult") || "");
+      setList(JSON.parse(localStorage.getItem("lastList") || "[]"));
+    }
+    handleStorageChange();
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, [lastResult, list]);
 
   const handleAdd = (item: string) => {
+    if (item === "") {
+      return;
+    }
     if (list.includes(item)) {
       return;
     }
+    localStorage.setItem("lastList", JSON.stringify([...list, item]));
     setList([...list, item]);
     setItem("");
   };
 
   const handleDelete = (item: string) => {
+    localStorage.setItem(
+      "lastList",
+      JSON.stringify(list.filter((i) => i !== item))
+    );
     setList(list.filter((i) => i !== item));
   };
 
@@ -22,11 +47,19 @@ export default function Home() {
     if (list.length === 0) {
       return;
     }
-    setResult(list[Math.floor(Math.random() * list.length)]);
+    const lastResult = localStorage.getItem("lastResult");
+    const filteredList = list.filter((i) => i !== lastResult);
+    const picked =
+      filteredList[Math.floor(Math.random() * filteredList.length)];
+    if (picked) {
+      setResult(picked);
+      localStorage.setItem("lastResult", picked);
+      setLastResult(picked);
+    }
   };
 
   return (
-    <div className="flex flex-col items-center gap-12">
+    <div className="flex flex-col items-center gap-12 w-96">
       <h1 className="text-3xl bg-emerald-200 shadow-lg rounded p-2 m-4 mt-16">
         Random Picker
       </h1>
@@ -60,7 +93,7 @@ export default function Home() {
       >
         추가하기
       </button>
-      <h3 className="text-xl bg-emerald-200 shadow-lg rounded p-2 m-4 mt-16">
+      <h3 className="text-xl min-w-[50%] min-h-16 border-[1.5px] border-emerald-700 shadow-lg rounded p-2 m-4 mt-16">
         {result}
       </h3>
       <button
@@ -69,6 +102,7 @@ export default function Home() {
       >
         뽑기
       </button>
+      {lastResult && <p>{lastResult}는 제외하고 뽑습니다</p>}
     </div>
   );
 }
